7b3f9c986702ff9e83058e2c89e998cabfcdcf90
Brian Bianchi <bianchibrian2@gmail.com>
2025-03-01 17:10:12 -0500

    Get riddle with one answer

--- LLMAPI/LLMAPI.py
+++ LLMAPI/LLMAPI.py
@@ -8,7 +8,7 @@ genai.configure(api_key = key)  # get's your key
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")  # replace with your model
 
 prompt_parts = [
-  "Describe a flower"  # text prompt (can be before, after, or interleaved)
+  "Give me a riddle with four multiple choice answers where only one is right"  # text prompt (can be before, after, or interleaved)
 ]
 
 response = model.generate_content(prompt_parts)  # the actual call
