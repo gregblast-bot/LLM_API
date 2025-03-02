4161e7904f4e2d4b2c9dbf7044a489cebf1e37e2
Gregory Wagonblast <ghw3@pitt.edu>
2025-03-01 19:07:13 -0500

    Update with Gemini API as env variable.

--- LLMAPI/LLMAPI.py
+++ LLMAPI/LLMAPI.py
@@ -1,14 +1,12 @@
 import google.generativeai as genai
 import os
 
-api_file = open("GeminiAPIKey/APIKey.txt", "r")
-key = api_file.readline()
-genai.configure(api_key = key)  # get's your key
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])  # get's your key
 
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")  # replace with your model
 
 prompt_parts = [
-  "Give me a riddle with four multiple choice answers where only one is right"  # text prompt (can be before, after, or interleaved)
+  "Why is the sky blue?"  # text prompt (can be before, after, or interleaved)
 ]
 
 response = model.generate_content(prompt_parts)  # the actual call
